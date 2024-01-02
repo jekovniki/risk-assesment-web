6bd1299dac31d31f60995badb943737b14561984
jekovniki <jekovniki95@gmail.com>
2024-01-02 22:55:55 +0200

    Tabs and stuff

--- src/pages/pep/index.js
+++ src/pages/pep/index.js
@@ -12,6 +12,7 @@ import { StandardBox } from '../../components/StandardBox';
 import Loader from '../../components/Loader';
 import PepBox from '../../components/PepBox';
 import { DEFAULT_IMAGE } from '../../utils/constants';
+import { InternalTabs } from '../../components/InternalTabs';
 
 function PEP() {
     const [loading, setLoading] = useState(true);
@@ -21,7 +22,7 @@ function PEP() {
     const id  = params.pathname.split('/pep/')[1];
     const [peps, setPeps] = useState([]);
     const data = peps.find(pep => pep._id === id);
-    
+    console.log('filteredData:', data);
     useEffect(() => {
         setLoading(true);
         getAllPeps.executeService();
@@ -37,6 +38,57 @@ function PEP() {
         }
     }, [getAllPeps.result, getAllPeps.error])
 
+    const getTabs = () => {
+        return [{
+            label: "Key Data",
+            text: <div>
+                <div style={{display: "flex", marginBottom: "1rem"}}>
+                    <div style={{flex: 1, color: "rgba(68, 83, 114, 1)"}}>Key Data</div>
+                    <div style={{flex: 3}}></div>
+                </div>
+                <div style={{display: "flex", marginBottom: "1rem"}}>
+                    <div style={{flex: 1, color: "rgba(68, 83, 114, 1)"}}>Name</div>
+                    <div style={{flex: 3}}>{data.name}</div>
+                </div>
+                <div style={{display: "flex", marginBottom: "1rem"}}>
+                    <div style={{flex: 1, color: "rgba(68, 83, 114, 1)"}}>Record Update</div>
+                    <div style={{flex: 3}}></div>
+                </div>
+                <div style={{display: "flex", marginBottom: "1rem"}}>
+                    <div style={{flex: 1, color: "rgba(68, 83, 114, 1)"}}>Category</div>
+                    <div style={{flex: 3}}>Political Individual</div>
+                </div>
+                <div style={{display: "flex", marginBottom: "1rem"}}>
+                    <div style={{flex: 1, color: "rgba(68, 83, 114, 1)"}}>PEP Status</div>
+                    <div style={{flex: 3}}>Active</div>
+                </div>
+                <div style={{display: "flex", marginBottom: "1rem"}}>
+                    <div style={{flex: 1, color: "rgba(68, 83, 114, 1)"}}>Special Interest Categories</div>
+                    <div style={{flex: 3}}></div>
+                </div>
+            </div>
+        }, {
+            label: "Further Information",
+            text: "No information"
+        }, {
+            label: "Aliases",
+            text: "No information"
+        }, {
+            label: "Keywords",
+            text: "No information"
+        }, {
+            label: "Pep Role Details",
+            text: "No information"
+        }, {
+            label: "Connections/Relationships",
+            text: "No information"
+        }, {
+            label: "Sources",
+            text: <div style={{fontSize: "1rem"}}>
+                <p><strong>Main source:</strong> Commission for confiscation of illegally acquired property <a href="https://www.caciaf.bg" target="_blank" rel="noreferrer">www.caciaf.bg</a></p>
+            </div>
+        }]
+    }
     return (
         <DashboardLayout>
             <DashboardNavbar />
@@ -56,6 +108,9 @@ function PEP() {
                                 citizenship={data?.citizenship ?? "Bulgarian"}
                             />
                         </StandardBox>
+                        <SoftBox mt={2}>
+                            <InternalTabs data={getTabs()} />
+                        </SoftBox>
                     </SoftBox>
                 }
                 <SoftBox>
